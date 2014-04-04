8232bd61afc70d753600e9b46fdbad23bd5a7826
Nick K <u@u-net.in>
2014-04-04 22:17:44 +0400

    Added ContainsAnyOf([]) check, GetAnySetting doesn't suck ass anymore

--- Section.cs
+++ Section.cs
@@ -58,22 +58,16 @@ namespace FLDataFile
         /// <summary>
         /// Returns any setting found with the names provided. At least two names needed, obviously.
         /// </summary>
-        /// <param name="setting1"></param>
-        /// <param name="setting2"></param>
-        /// <param name="setting3"></param>
-        /// <param name="setting4"></param>
+        /// <param name="settings">Setting names.</param>
         /// <returns>First setting found, or null if none found.</returns>
-        public Setting GetAnySetting(string setting1, string setting2,string setting3 = null, string setting4 = null)
+        public Setting GetAnySetting(params string[] settings)
         {
-            var gf1 = GetFirstOf(setting1);
-            if (gf1 != null) return gf1;
-            var gf2 = GetFirstOf(setting2);
-            if (gf2 != null) return gf2;
-            var gf3 = GetFirstOf(setting3);
-            if (gf3 != null) return gf3;
-            var gf4 = GetFirstOf(setting4);
-            if (gf4 != null) return gf4;
-            return null;
+            return settings.Select(GetFirstOf).FirstOrDefault(set => set != null);
+        }
+
+        public bool ContainsAnyOf(params string[] settings)
+        {
+            return settings.Any(set => Settings.Any(w => w.Name == set));
         }
 
         /// <summary>
